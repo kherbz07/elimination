a3f1cd634e9a7660488d3378f4476dbb7b74abbe
Kirby <kherbz07@gmail.com>
2015-03-14 17:02:34 +0800

    Natapos din nga di P***

--- recommendation.php
+++ recommendation.php
@@ -1,80 +1,72 @@
 <?php
-new Recommend();
 
-class Recommend
+$argc = $_SERVER['argc'];
+$argv = $_SERVER['argv'];
+
+$input = '';
+$base = '';
+$recommendation = '';
+
+if ($argc >= 2)
 {
-	private $input;
-	private $base;
-	private $recommendation;
+	$input = $argv[1];
+	$base = getBase($input);
+	$recommendation = recommendUsername($input, $base);
 
-	public function __construct()
-	{
-		$this->recommendation = '';
-
-		$argc = $_SERVER['argc'];
-		$argv = $_SERVER['argv'];
-
-		if ($argc >= 2)
-		{
-			$this->input = $argv[1];
-			$this->base = $this->getBase($this->input);
-			$this->recommendation = $this->recommendUsername($this->input);
-		}
-		$this->index();
-	}
-
-	public function index()
-	{
-		print('Input: ' . $this->input . "\n");
-		print('Recommendation: ' . $this->recommendation);
-	}
-
-	public function recommendUsername($username)
-	{
-		$flag = true;
-		$ctr = 0;
-
-		while ($flag)
-		{
-			$isFound = false;
-
-			$users_file = fopen("usernames.txt", "r") or die('Unable to open file.');
-			while (!feof($users_file) && !$isFound)
-			{
-				$old_user = fgets($users_file);
-				$old_user = substr($old_user, 0, -2);
-				if ($username == $old_user)
-				{
-					$username = $this->base;
-					$username .= $ctr;
-					$ctr++;
-					$isFound = true;
-				}
-			}
-			$old_user = fgets($users_file);
-			fclose($users_file);
-
-			if (!$isFound)
-			{
-				$flag = false;
-			}
-		}
-
-		return $username;
-	}
-
-	public function getBase($username)
-	{
-		$flag = true;
-		$count = strlen($username);
-		for ($i = 0; $i < $count && $flag; $i++)
-		{
-			if (ctype_digit($username[$i]))
-			{
-				$flag = false;
-				$username = substr($username, 0, ($i));
-			}
-		}
-		return $username;
-	}
+	print('Input: ' . $input . "\n");
+	print('Recommendation: ' . $recommendation);
 }
+else
+{
+	print('No input.');
+}
+
+function recommendUsername($username, $base)
+{
+	$flag = true;
+	$ctr = 0;
+
+	while ($flag)
+	{
+		$isFound = false;
+
+		$users_file = fopen("usernames.txt", "r") or die('Unable to open file.');
+		while (!feof($users_file) && !$isFound)
+		{
+			$old_user = fgets($users_file);
+			$old_user = preg_replace("/[^A-Za-z0-9]/", '', $old_user);
+			if ($username == $old_user)
+			{
+				$username = $base;
+				$username .= $ctr;
+				$ctr++;
+				$isFound = true;
+			}
+		}
+		$old_user = fgets($users_file);
+		fclose($users_file);
+
+		if (!$isFound)
+		{
+			$flag = false;
+		}
+	}
+
+	return $username;
+}
+
+function getBase($username)
+{
+	$flag = true;
+	$count = strlen($username);
+	for ($i = 0; $i < $count && $flag; $i++)
+	{
+		if (ctype_digit($username[$i]))
+		{
+			$flag = false;
+			$username = substr($username, 0, ($i));
+		}
+	}
+	return $username;
+}
+?>
